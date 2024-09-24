bc427ae1c6327d1cc0652efe8ac6b030bfb8b037
varunshivam <138372856+varunshivam@users.noreply.github.com>
2024-09-24 19:39:05 +0000

    dataconversion

---
+++ .devcontainer/basics/dataconversion.js
@@ -0,0 +1,13 @@
+let score ="33abs"
+console.log(typeof score)
+let valueINstring=Number(score)
+console.log(valueINstring)
+console.log(typeof valueINstring)
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1;,false=>0
+let isLoggedIN=1
+let boolLoggedIn=Boolean(isLoggedIN)
+console.log(boolLoggedIn);
+console.log(typeof boolLoggedIn)
