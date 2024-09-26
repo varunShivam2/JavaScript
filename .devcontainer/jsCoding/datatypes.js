39ef1d4ef6a7ea4e1960e444b4e4a2e746e58ba5
varunshivam <138372856+varunshivam@users.noreply.github.com>
2024-09-26 17:10:34 +0000

    datarypesNonPrimitive

---
+++ .devcontainer/jsCoding/datatypes.js
@@ -0,0 +1,24 @@
+const myNum=33322
+
+console.log(myNum);
+
+let myObj={
+    name: "varun",
+    age:23
+}
+// console.log(myObj.name);
+const arr1=["varun","shivam"]
+ arr1[0]="tarun"
+for (let index = 0; index < arr1.length; index++) {
+    const element = arr1[index];
+  
+    console.log(arr1[index]);
+    
+}
+
+function heyy() {
+    console.log("Hello guys");
+    
+}
+heyy()
+
